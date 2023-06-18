5c6df212f01a8f334355de5208298517b6b65f03
luvvas <lucasmachado1@ufpr.br>
2023-06-18 08:05:46 -0300

    feat: add avatar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,22 @@
 import { signOut, useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 import { SignIn, SignOut } from "@phosphor-icons/react"
 
 import { Navigation } from "../Navigation"
 import { Text } from '../../components/typography'
+import { Avatar } from "../ui/Avatar"
 
 import { Container, LoginButton, UserDetails } from "./styles"
 
 export const Sidebar = () => {
   const { data: session } = useSession()
+  const router = useRouter()
 
   const user = session?.user;
+
+  const handleOpenProfile = () => {
+    router.push(`/profile/${user?.id}`)
+  }
 
   return (
     <Container>
@@ -27,7 +34,7 @@ export const Sidebar = () => {
           </LoginButton>
         ) : (
           <UserDetails>
-            {/* FALTA AVATAR */}
+            <Avatar  size="sm" src={user?.avatar_url} alt={user?.name} onClick={() => handleOpenProfile()} css={{ cursor: "pointer" }}/>
             <Text size="sm">{user?.name}</Text>
             <SignOut color="#F75A67" onClick={() => signOut()}/>
           </UserDetails>
