e35d8777b63d667308e521dd02c8c5409dc4fbce
luvvas <lucasmachado1@ufpr.br>
2023-06-27 16:51:44 -0300

    feat: add book card component in explore

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,11 +1,13 @@
 import { Book } from "@prisma/client"
 
 import { RatingStars } from "../RatingStars";
-import { BookDetails, BookImage, BookName, Container } from "./styles"
+import { BookDetails, BookImage, BookName, Container, ReadBadge } from "./styles"
 import { Text } from "../typography";
+import { RatingsDialog } from "../ui/RatingsDialog";
 
 export type BookWithAvgRating = Book & {
   avgRating: number
+  alreadyRead: boolean
 }
 
 type BookCardProps = {
@@ -28,26 +30,31 @@ export const BookCard = ({ book, size = "md" }: BookCardProps) => {
   const currentSize = IMAGE_SIZES[size]
 
   return (
-    <Container>
-      <BookImage
-        src={book.cover_url} 
-        width={currentSize.width} 
-        height={currentSize.height}
-        css={{ minWidth: currentSize.width }} 
-        alt={book.name} 
-      />
-      <BookDetails>
-        <div>
-          <BookName size="xs">
-            {book.name}
-          </BookName>
-          <Text size="sm" color="gray-400">
-            {book.author}
-          </Text>
-        </div>
+    <RatingsDialog>
+      <Container>
+        {book?.alreadyRead && (
+          <ReadBadge>LIDO</ReadBadge>
+        )}
+        <BookImage
+          src={book.cover_url} 
+          width={currentSize.width} 
+          height={currentSize.height}
+          css={{ minWidth: currentSize.width }} 
+          alt={book.name} 
+        />
+        <BookDetails>
+          <div>
+            <BookName size="xs">
+              {book.name}
+            </BookName>
+            <Text size="sm" color="gray-400">
+              {book.author}
+            </Text>
+          </div>
 
-        <RatingStars rating={book.avgRating}/>
-      </BookDetails>
-    </Container>
+          <RatingStars rating={book.avgRating}/>
+        </BookDetails>
+      </Container>
+    </RatingsDialog>
   )
 }
